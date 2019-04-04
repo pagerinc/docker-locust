b0f348fd2c67fe56ce7e9e4c898373a126cdf3c2
Gaston Festari <cilindrox@gmail.com>
2019-04-04 16:36:27 -0300

    test: update runner

---
+++ test/run.sh
@@ -0,0 +1,14 @@
+#!/bin/ash
+
+set -o errexit
+set -o nounset
+
+apt-get update && apt-get install --no-install-recommends -y bats
+
+echo "Unit Tests..."
+
+bats /tmp/test
+
+echo "#############"
+echo "# Tests OK! #"
+echo "#############"
